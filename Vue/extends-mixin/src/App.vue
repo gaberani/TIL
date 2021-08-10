1086ee9ca8a7799a003ea544be98a90650d03252
gaberani <khs0783@naver.com>
2021-08-10 23:55:22 +0900

    Test: created, mounted console test about mixin, extends

--- Vue/extends-mixin/src/App.vue
+++ Vue/extends-mixin/src/App.vue
@@ -1,17 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <test-mixin-vue-life-cycle></test-mixin-vue-life-cycle>
+    <test-extends-vue-life-cycle></test-extends-vue-life-cycle>
+
+    <!-- <template id="test-mixin-vue-life-cycle">
+      <h1>
+        Mixin
+      </h1>
+    </template>
+    <template id="test-extends-vue-life-cycle">
+      <h1>
+        Extends
+      </h1>
+    </template> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+
+const mixin1 = {
+  created() {
+    console.log('Created Mixin 1');
+  }
+}
+
+const mixin2 = {
+  created() {
+    console.log('Created Mixin 2');
+  }
+}
+
+const mixin3 = {
+  mounted() {
+    console.log('Mounted Mixin 3');
+  }
+}
+
+const mixin4 = {
+  mounted() {
+    console.log('Mounted Mixin 4');
+  }
+}
+
+Vue.component('test-mixin-vue-life-cycle', {
+  template: "#test-mixin-vue-life-cycle",
+  mixins: [mixin1, mixin2, mixin3, mixin4],
+  created() {
+    console.log('Created from the child!');
+  },
+  mounted() {
+    console.log('Mounted from the child!');
+  },
+  render() {
+    return (
+      <template id="test-mixin-vue-life-cycle">
+        <h1>
+          Mixin
+        </h1>
+      </template>
+    )
+  }
+})
+
+/*
+	Extends 
+*/
+const ParentComponent = {
+  created() {
+    console.log('[Extends] Created from the parent!');
+  }
+}
+
+Vue.component('test-extends-vue-life-cycle', {
+	template: "#test-extends-vue-life-cycle",
+  extends: ParentComponent,
+  created() {
+    console.log('[Extends] Created from the child!');
+  },
+  render() {
+    return (
+      <template id="test-extends-vue-life-cycle">
+        <h1>
+          Extends
+        </h1>
+      </template>
+    )
+  }
+})
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
